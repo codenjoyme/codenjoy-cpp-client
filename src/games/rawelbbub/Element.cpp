94faddb59bd956851e08d1d7d2928a8012f4205d
Oleksandr Baglai <oleksandr_baglai@epam.com>
2022-01-03 01:17:49 +0200

    [rawelbbub][clients][all] Refactoring. Rename ICE -> OIL.

--- src/games/rawelbbub/Element.cpp
+++ src/games/rawelbbub/Element.cpp
@@ -70,11 +70,12 @@ ElementMap Element::initialiseElements() {
 
     result[LL("EXPLOSION")] = LL('Ѡ');
 
-        // Ice, having driven onto which hero will begin to drift.
-        // During a skid, hero will repeat the old commands for several
-        // ticks in a row, ignoring the current commands.
+        // Oil leak, hitting which the hero partially loses control.
+        // During the passage, the field of view is limited and the
+        // hero will repeat the old commands for several ticks in a
+        // row, ignoring the current commands.
 
-    result[LL("ICE")] = LL('#');
+    result[LL("OIL")] = LL('#');
 
         // The trees hide heroes which can continue to shoot at the
         // same time. The fired shells are also not visible under the
